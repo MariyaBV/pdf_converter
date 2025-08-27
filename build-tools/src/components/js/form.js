06aa69594ac936a0fbcf74b2dc67697787974f39
Mariia Beliakova <mari.mv2008@gmail.com>
2025-08-27 15:36:00 +0200

    login

--- build-tools/src/components/js/form.js
+++ build-tools/src/components/js/form.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function ($) {
         return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
     }
     // Обработка отправки формы
-    $('#message-contact').on('submit', function (e) {
+    $('#form-registration').on('submit', function (e) {
         e.preventDefault();
 
         const email = $('input[name="email"]').val().trim();
@@ -24,7 +24,7 @@ jQuery(document).ready(function ($) {
         const msg = messages[lang] || messages['en'];
 
         // Проверки
-        if (!email || !phone || !isValidEmail(email)) {
+        if (!email || !isValidEmail(email)) {
             e.preventDefault();
             alert(msg.fill);
             return false;
@@ -39,4 +39,39 @@ jQuery(document).ready(function ($) {
         // Если всё ок то форма отправляется обычным POST на сервер
     });
 
+    $('#form-login').on('submit', function (e) {
+        e.preventDefault();
+
+        const email = $('input[name="email"]').val().trim();
+        const password = $('input[name="password"]').val().trim();
+        const recaptcha = grecaptcha.getResponse();
+
+        const lang = document.documentElement.lang;
+        const messages = {
+            en: {
+                fill: "Please enter a valid email and password.",
+                captcha: "Please confirm you are not a robot."
+            },
+            ru: {
+                fill: "Пожалуйста, введите корректные email и телефон.",
+                captcha: "Пожалуйста, подтвердите, что вы не робот."
+            },
+        };
+        const msg = messages[lang] || messages['en'];
+
+        // Проверки
+        if (!email || !password || !isValidEmail(email)) {
+            e.preventDefault();
+            alert(msg.fill);
+            return false;
+        }
+
+        if (recaptcha.length === 0) {
+            e.preventDefault();
+            alert(msg.captcha);
+            return false;
+        }
+
+        // Если всё ок то форма отправляется обычным POST на сервер
+    });
 });
